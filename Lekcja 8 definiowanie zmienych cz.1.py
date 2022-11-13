d9f91d3f86c07c00aa8a6f81caa2d99b58eb6b36
Szadok1995 <117068953+Szadok1995@users.noreply.github.com>
2022-11-13 15:52:06 +0100

    Add files via upload

---
+++ Lekcja 8 definiowanie zmienych cz.1.py
@@ -0,0 +1,12 @@
+'''
+Lekcja 8 Czym jest średnik i enter dla interpretera?
+'''
+x = 5
+y = 2
+z = 3
+#średnik oznacza, że w tym miejscu konczy sie instrukcja dla komputera
+x = 5;  y = 2; z = 3;   
+
+a,b,c = 1, 5, 7
+
+d = e = f = 44
